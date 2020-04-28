8c8182ead0ad66279b019066c3000099f898cb1e
aliasad <aliasad059@gmail.com>
2020-04-28 15:17:34 +0430

    body tab is added

--- Insomnia/src/Display.java
+++ Insomnia/src/Display.java
@@ -93,9 +93,107 @@ public class Display {
 
     private void makeNewRequestPanel() {
         newRequestPanel = new JPanel();
+        newRequestPanel.setLayout(new BorderLayout());
+        newRequestPanel.setBackground(Color.GRAY);
+        JPanel north = new JPanel();
+        north.setLayout(new GridLayout(1, 3));
+        String[] methodsName = {"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"};
+        north.add(new JComboBox(methodsName));
+        north.add(new JTextField());
+        north.add(new JButton("Send"));
+
+        newRequestPanel.add(north, BorderLayout.NORTH);
+
+        JTabbedPane tabs = new JTabbedPane();
+        JPanel bodyPanel = new JPanel();
+        bodyPanel.setLayout(new BorderLayout());
+        JPanel authPanel = new JPanel();
+        JPanel queryPanel = new JPanel();
+        JPanel headerPanel = new JPanel();
+        JPanel docsPanel = new JPanel();
+
+        tabs.addTab("Body", bodyPanel);
+        tabs.addTab("Auth", authPanel);
+        tabs.addTab("Query", queryPanel);
+        tabs.addTab("Header", headerPanel);
+        tabs.addTab("Docs", docsPanel);
+
+
+        ////////////////////////////// making body panel
+
+
+        String[] bodyTab = {"No body", "Form", "JASON", "Binary"};
+        JComboBox bodyTabStatus = new JComboBox(bodyTab);
+        bodyPanel.add(bodyTabStatus, BorderLayout.NORTH);
+
+        JPanel binaryPanel = new JPanel();
+        binaryPanel.setLayout(new BorderLayout());
+        JButton fileChooserButton = new JButton("Choose file");
+        binaryPanel.add(fileChooserButton, BorderLayout.NORTH);
+        final JTextField[] filePath = new JTextField[1];
+
+        fileChooserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fc = new JFileChooser();
+                int i = fc.showOpenDialog(null);
+                if (i == JFileChooser.APPROVE_OPTION) {
+                    File f = fc.getSelectedFile();
+                    String filepath = f.getPath();
+                    JOptionPane.showMessageDialog(null, filepath, "You have chosen following file ...", 1);
+                    filePath[0] = new JTextField(filepath);
+                    frame.validate();
+                }
+            }
+        });
+
+        JPanel JSONPanel = new JPanel(new BorderLayout());
+        JTextArea JSONEditor = new JTextArea("");
+        JScrollPane sp = new JScrollPane(JSONEditor);
+        JSONPanel.add(sp, BorderLayout.CENTER);
+
+        JPanel formPanel = new JPanel();
+
+        JPanel noBodyPanel = new JPanel();
+
+        bodyPanel.add(noBodyPanel);
+
+        bodyTabStatus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComboBox cb = (JComboBox) e.getSource();
+                if (cb.getSelectedIndex() == 1) {
+                    System.out.println(1);
+                    bodyPanel.remove(1);
+                    bodyPanel.add(formPanel, BorderLayout.CENTER);
+                } else if (cb.getSelectedIndex() == 2) {
+                    System.out.println(2);
+                    bodyPanel.remove(1);
+                    bodyPanel.add(JSONPanel, BorderLayout.CENTER);
+                } else if (cb.getSelectedIndex() == 3) {
+                    System.out.println(3);
+                    bodyPanel.remove(1);
+                    bodyPanel.add(binaryPanel, BorderLayout.CENTER);
+                } else {
+                    System.out.println(0);
+                    bodyPanel.remove(1);
+                    bodyPanel.add(noBodyPanel, BorderLayout.CENTER);
+                }
+                frame.validate();
+            }
+        });
+        //////////////////////////////////////////
+
+
+        newRequestPanel.add(tabs, BorderLayout.CENTER);
+
+
     }
     private void makeResponsePanel() {
         responsePanel = new JPanel();
+        responsePanel.setBackground(Color.DARK_GRAY);
+        //responsePanel.setBounds(2*FRAME_WIDTH/3,0,FRAME_WIDTH/3,FRAME_HEIGHT);
+        responsePanel.setLayout(new BorderLayout());
     }
 
     private void darkTheme() {
